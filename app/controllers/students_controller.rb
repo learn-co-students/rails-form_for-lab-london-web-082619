dc7470202e71d63cac70a35ef92b401bbbf85b61
github username <github email address>
2019-09-22 19:37:55 +0100

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,39 @@
+class StudentsController < ApplicationController
+
+    def index
+    @students = Student.all
+    end
+
+    def new
+        @student = Student.new # ここでインスタンスを作っておき、New formにパスすることで、NewとEditの分岐を行えるようにしておく。New formをみよ。
+    end
+  
+    def show
+        @student = Student.find([:id])
+    end
+
+    def create
+        @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+        redirect_to student_path(@student)
+    end
+
+    def edit
+        @student = Student.find([:id])
+    end
+
+    def update
+        @student = Student.find([:id])
+        @student.update(student_params)
+        redirect_to student_path(@student)
+    end
+
+    def delete
+       Studen.destroy(params[:id])
+    end
+
+    private
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name)
+    end
+end
